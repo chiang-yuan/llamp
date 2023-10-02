bfc7fee5fe80ba0a091cc6b8b626f39fa03e32fe
knhn1004 <49494541+knhn1004@users.noreply.github.com>
2023-10-02 09:29:08 -0700

    extract Message

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,26 +1,9 @@
 <script lang="ts">
+  import Message from './Message.svelte';
+
 	import { Avatar, CodeBlock, ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
 	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
 	import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-	interface MessageFeed {
-		id: number;
-		host: boolean;
-		avatar: number;
-		name: string;
-		timestamp: string;
-		message: string;
-		color: string;
-	};
-
-	const bubble: MessageFeed= {
-		id: 1,
-		host: true,
-		avatar: 1,
-		name: 'LLaMP',
-		timestamp: '2 hours ago',
-		message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam libero ultricies nunc, nec aliquet nisl nunc eu nunc. Nulla facil',
-		color: 'primary',
-	}
 
 	// for display purposes only
 	interface Question {
@@ -63,44 +46,35 @@
 
 
 <div class="chat w-full h-full grid grid-cols-1 lg:grid-cols-[30%_1fr]">
-<!-- Navigation -->
-<div class="hidden card lg:grid grid-rows-[auto_1fr_auto] border-r border-surface-500/30">
-	<!-- Header -->
-	<header class="border-b border-surface-500/30 p-4">
-		<input class="input" type="search" placeholder="Search History" />
-	</header>
-	<!-- List -->
-	<div class="p-4 space-y-4 overflow-y-auto">
-		<small class="opacity-50">Chat History</small>
-		<ListBox active="variant-filled-primary">
-			{#each questions as question}
-				<ListBoxItem bind:group={currentQuestion} name="questions" value={question.title}>
-					<svelte:fragment slot="lead">
-						<Avatar width="w-8" initials={question.timeElapsedInitials}/>
-					</svelte:fragment>
-					{question.title.slice(0, 50) + '...'}
-				</ListBoxItem>
-			{/each}
-		</ListBox>
+	<!-- Navigation -->
+	<div class="hidden card lg:grid grid-rows-[auto_1fr_auto] border-r border-surface-500/30">
+		<!-- Header -->
+		<header class="border-b border-surface-500/30 p-4">
+			<input class="input" type="search" placeholder="Search History" />
+		</header>
+		<!-- List -->
+		<div class="p-4 space-y-4 overflow-y-auto">
+			<small class="opacity-50">Chat History</small>
+			<ListBox active="variant-filled-primary">
+				{#each questions as question}
+					<ListBoxItem bind:group={currentQuestion} name="questions" value={question.title}>
+						<svelte:fragment slot="lead">
+							<Avatar width="w-8" initials={question.timeElapsedInitials}/>
+						</svelte:fragment>
+						{question.title.slice(0, 50) + '...'}
+					</ListBoxItem>
+				{/each}
+			</ListBox>
+		</div>
+		<!-- Footer -->
+		<footer class="border-t border-surface-500/30 p-4 opacity-50">LLaMP Project All Rights Reserved.</footer>
 	</div>
-	<!-- Footer -->
-	<!-- <footer class="border-t border-surface-500/30 p-4">(footer)</footer> -->
-</div>
 
 <!-- Chat -->
-<div class="flex flex-col h-full">
-	<!-- Conversation -->
-	<section class="p-4 overflow-y-auto flex-grow space-y-4">
-				<div class="grid grid-cols-[auto_1fr] gap-2">
-					<Avatar width="w-12" initials="MP"/>
-					<div class="card p-4 variant-soft rounded-tl-none space-y-2">
-						<header class="flex justify-between items-center">
-							<p class="font-bold">{bubble.name}</p>
-							<small class="opacity-50">{bubble.timestamp}</small>
-						</header>
-						<p>{bubble.message}</p>
-					</div>
-				</div>
+	<div class="flex flex-col h-full">
+		<!-- Conversation -->
+		<section class="p-4 overflow-y-auto flex-grow space-y-4">
+			<Message/>
 		</section>
 		<!-- Prompt -->
 		<section class="card border-t border-surface-500/30 p-4">
---
+++ web/src/routes/Message.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton';
+	interface MessageFeed {
+		id: number;
+		host: boolean;
+		avatar: number;
+		name: string;
+		timestamp: string;
+		message: string;
+		color: string;
+	};
+
+	const bubble: MessageFeed= {
+		id: 1,
+		host: true,
+		avatar: 1,
+		name: 'LLaMP',
+		timestamp: '2 hours ago',
+		message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam libero ultricies nunc, nec aliquet nisl nunc eu nunc. Nulla facil',
+		color: 'primary',
+	}
+
+</script>
+
+<div class="grid grid-cols-[auto_1fr] gap-2">
+<Avatar width="w-12" initials="MP"/>
+<div class="card p-4 variant-soft rounded-tl-none space-y-2">
+	<header class="flex justify-between items-center">
+		<p class="font-bold">{bubble.name}</p>
+		<small class="opacity-50">{bubble.timestamp}</small>
+	</header>
+	<p>{bubble.message}</p>
+</div>
+</div>
+
