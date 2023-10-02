8c5c649fd69c8a7733d6d951817ed345c0df5dc0
knhn1004 <49494541+knhn1004@users.noreply.github.com>
2023-10-02 09:23:38 -0700

    chat UI w/o backend

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-
-import { Avatar, CodeBlock, ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
+	import { Avatar, CodeBlock, ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
+	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+	import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 	interface MessageFeed {
 		id: number;
 		host: boolean;
@@ -60,64 +61,63 @@ import { Avatar, CodeBlock, ListBox, ListBoxItem } from '@skeletonlabs/skeleton'
 
 
 
-<section class="flex-grow">
-	<div class="chat w-full h-full grid grid-cols-1 lg:grid-cols-[30%_1fr]">
-		<!-- Navigation -->
-		<div class="hidden card lg:grid grid-rows-[auto_1fr_auto] border-r border-surface-500/30">
-			<!-- Header -->
-			<header class="border-b border-surface-500/30 p-4">
-				<input class="input" type="search" placeholder="Search..." />
-			</header>
-			<!-- List -->
-			<div class="p-4 space-y-4 overflow-y-auto">
-				<small class="opacity-50">Chat History</small>
-				<ListBox active="variant-filled-primary">
-					{#each questions as question}
-						<ListBoxItem bind:group={currentQuestion} name="questions" value={question.title}>
-							<svelte:fragment slot="lead">
-								<Avatar width="w-8" initials={question.timeElapsedInitials}/>
-							</svelte:fragment>
-							{question.title.slice(0, 50) + '...'}
-						</ListBoxItem>
-					{/each}
-				</ListBox>
-			</div>
-			<!-- Footer -->
-			<!-- <footer class="border-t border-surface-500/30 p-4">(footer)</footer> -->
-		</div>
 
-		<!-- Chat -->
-		<div class="flex flex-col h-full">
-			<!-- Conversation -->
-			<section class="p-4 overflow-y-auto flex-grow space-y-4">
-						<div class="grid grid-cols-[auto_1fr] gap-2">
-							<Avatar width="w-12" initials="MP"/>
-							<div class="card p-4 variant-soft rounded-tl-none space-y-2">
-								<header class="flex justify-between items-center">
-									<p class="font-bold">{bubble.name}</p>
-									<small class="opacity-50">{bubble.timestamp}</small>
-								</header>
-								<p>{bubble.message}</p>
-							</div>
-						</div>
-				</section>
-				<!-- Prompt -->
-				<section class="card border-t border-surface-500/30 p-4">
-					<div class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token">
-						<button class="input-group-shim">+</button>
-						<textarea
-							bind:value={currentMessage}
-							class="bg-transparent border-0 ring-0"
-							name="prompt"
-							id="prompt"
-							placeholder="Write a message..."
-							rows="1"
-						/>
-						<button class={currentMessage ? 'variant-filled-primary' : 'input-group-shim'} >
-							<i class="fa-solid fa-paper-plane" />
-						</button>
+<div class="chat w-full h-full grid grid-cols-1 lg:grid-cols-[30%_1fr]">
+<!-- Navigation -->
+<div class="hidden card lg:grid grid-rows-[auto_1fr_auto] border-r border-surface-500/30">
+	<!-- Header -->
+	<header class="border-b border-surface-500/30 p-4">
+		<input class="input" type="search" placeholder="Search History" />
+	</header>
+	<!-- List -->
+	<div class="p-4 space-y-4 overflow-y-auto">
+		<small class="opacity-50">Chat History</small>
+		<ListBox active="variant-filled-primary">
+			{#each questions as question}
+				<ListBoxItem bind:group={currentQuestion} name="questions" value={question.title}>
+					<svelte:fragment slot="lead">
+						<Avatar width="w-8" initials={question.timeElapsedInitials}/>
+					</svelte:fragment>
+					{question.title.slice(0, 50) + '...'}
+				</ListBoxItem>
+			{/each}
+		</ListBox>
+	</div>
+	<!-- Footer -->
+	<!-- <footer class="border-t border-surface-500/30 p-4">(footer)</footer> -->
+</div>
+
+<!-- Chat -->
+<div class="flex flex-col h-full">
+	<!-- Conversation -->
+	<section class="p-4 overflow-y-auto flex-grow space-y-4">
+				<div class="grid grid-cols-[auto_1fr] gap-2">
+					<Avatar width="w-12" initials="MP"/>
+					<div class="card p-4 variant-soft rounded-tl-none space-y-2">
+						<header class="flex justify-between items-center">
+							<p class="font-bold">{bubble.name}</p>
+							<small class="opacity-50">{bubble.timestamp}</small>
+						</header>
+						<p>{bubble.message}</p>
 					</div>
-				</section>
+				</div>
+		</section>
+		<!-- Prompt -->
+		<section class="card border-t border-surface-500/30 p-4">
+			<div class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token">
+				<button class="input-group-shim">+</button>
+				<textarea
+					bind:value={currentMessage}
+					class="bg-transparent border-0 ring-0"
+					name="prompt"
+					id="prompt"
+					placeholder="Ask a question..."
+					rows="1"
+				/>
+				<button class={currentMessage ? 'variant-filled-primary' : 'input-group-shim'} >
+					<FontAwesomeIcon icon={faPaperPlane} />
+				</button>
 			</div>
-		</div>
-	</section>
+		</section>
+	</div>
+</div>
