5030d3e611eb34bf2c5808ce4bb102ab9c49e064
knhn1004 <49494541+knhn1004@users.noreply.github.com>
2023-10-01 23:43:46 -0700

    added skeleton

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,12 +1,14 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: [ vitePreprocess()],
+	
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
@@ -14,5 +16,4 @@ const config = {
 		adapter: adapter()
 	}
 };
-
 export default config;
