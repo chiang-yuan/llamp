72317f7fa24cbb2b220922b0432e9b47a048693b
Yuan Chiang <qaqowotut@gmail.com>
2023-10-28 10:16:55 -0700

    update docker push shell

--- docker-push-nersc.sh
+++ docker-push-nersc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker build -t llamp-api-prod ./api
-docker tag llamp-api-prod:latest registry.nersc.gov/matgen/llamp/llamp-api-prod:latest
+docker build -t llamp-api-prod:test ./api
+docker tag llamp-api-prod:test registry.nersc.gov/matgen/llamp/llamp-api-prod:test
 
-docker build -t llamp-web-prod ./web
-docker tag llamp-web-prod:latest registry.nersc.gov/matgen/llamp/llamp-web-prod:latest
+docker build -t llamp-web-prod:test ./web
+docker tag llamp-web-prod:test registry.nersc.gov/matgen/llamp/llamp-web-prod:test
 
-docker push registry.nersc.gov/matgen/llamp/llamp-api-prod:latest
-docker push registry.nersc.gov/matgen/llamp/llamp-web-prod:latest
+docker push registry.nersc.gov/matgen/llamp/llamp-api-prod:test
+docker push registry.nersc.gov/matgen/llamp/llamp-web-prod:test
