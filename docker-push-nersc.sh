fb96433de7d11295d7aee64f7b0d171527efd94a
Yuan Chiang <qaqowotut@gmail.com>
2023-11-03 19:59:39 -0700

    rancher k8s test

--- docker-push-nersc.sh
+++ docker-push-nersc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker build --no-cache -t llamp-api:test ./api
+docker build --no-cache -t llamp-api:test -f ./api/Dockerfile ./api
 docker tag llamp-api:test registry.nersc.gov/matgen/llamp/llamp-api:test
 
-docker build --no-cache -t llamp-web:test ./web
+docker build --no-cache -t llamp-web:test -f ./web/Dockerfile ./web
 docker tag llamp-web:test registry.nersc.gov/matgen/llamp/llamp-web:test
 
 docker push registry.nersc.gov/matgen/llamp/llamp-api:test
