f0eff07ddd60e40d5d149f93a4f5b07b10c92e98
knhn1004 <49494541+knhn1004@users.noreply.github.com>
2024-03-06 19:33:56 -0800

    chat_id

--- api/src/llamp/sse.py
+++ api/src/llamp/sse.py
@@ -2,7 +2,6 @@ import asyncio
 import os
 import re
 import uuid
-from collections.abc import AsyncGenerator, Generator
 
 import redis
 import uvicorn
@@ -13,12 +12,7 @@ from dotenv import load_dotenv
 from fastapi.responses import StreamingResponse
 from langchain import hub
 from langchain.agents import AgentType, initialize_agent
-from langchain.agents.format_scratchpad import format_log_to_str
-from langchain.agents.output_parsers import (
-    JSONAgentOutputParser,
-)
 from langchain.callbacks.base import BaseCallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain.prompts import MessagesPlaceholder
 from langchain.tools import ArxivQueryRun, WikipediaQueryRun
@@ -48,84 +42,10 @@ OPENAI_GPT_MODEL = "gpt-3.5-turbo-0125"
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = os.getenv("REDIS_PORT", 6379)
 
-custom_callback_handler = StreamingRedisCallbackHandler(
-    redis_host=REDIS_HOST, redis_port=REDIS_PORT, redis_channel='llm_stream')  # TODO: different redis channel for each chat_id
-
-mp_llm = ChatOpenAI(
-    temperature=0,
-    model=OPENAI_GPT_MODEL,
-    openai_api_key=OPENAI_API_KEY,
-    openai_organization=None,
-    max_retries=5,
-    streaming=True,
-    callbacks=[custom_callback_handler],
-)
-
-llm = ChatOpenAI(
-    temperature=0,
-    model=OPENAI_GPT_MODEL,
-    openai_api_key=OPENAI_API_KEY,
-    openai_organization=None,
-    streaming=True,
-    callbacks=[custom_callback_handler],
-)
 
 wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
 arxiv = ArxivQueryRun(api_wrapper=ArxivAPIWrapper())
 
-tools = [
-    MPThermoExpert(llm=mp_llm).as_tool(
-        agent_kwargs=dict(return_intermediate_steps=False)),
-    MPElasticityExpert(llm=mp_llm).as_tool(
-        agent_kwargs=dict(return_intermediate_steps=False)),
-    MPDielectricExpert(llm=mp_llm).as_tool(
-        agent_kwargs=dict(return_intermediate_steps=False)),
-    MPMagnetismExpert(llm=mp_llm).as_tool(
-        agent_kwargs=dict(return_intermediate_steps=False)),
-    MPElectronicExpert(llm=mp_llm).as_tool(
-        agent_kwargs=dict(return_intermediate_steps=False)),
-    MPPiezoelectricExpert(llm=mp_llm).as_tool(
-        agent_kwargs=dict(return_intermediate_steps=False)),
-    MPSummaryExpert(llm=mp_llm).as_tool(
-        agent_kwargs=dict(return_intermediate_steps=False)),
-    MPSynthesisExpert(llm=mp_llm).as_tool(
-        agent_kwargs=dict(return_intermediate_steps=False)),
-    MPStructureRetriever(llm=mp_llm).as_tool(
-        agent_kwargs=dict(return_intermediate_steps=True)),
-    arxiv,
-    wikipedia,
-]
-
-prompt = hub.pull("hwchase17/react-multi-input-json")
-# prompt = hub.pull("hwchase17/structured-chat-agent")
-# prompt = hub.pull("hwchase17/react")
-prompt.messages[0].prompt.template = re.sub(
-    r"\s+", " ",
-    """You are a data-aware agent that can consult materials-related
-    data through Materials Project (MP) database, arXiv, and Wikipedia. Ask 
-    user to clarify their queries if needed. Please note that you don't have 
-    direct control over MP but through multiple assistant agents to help you. 
-    You need to provide complete context in the input for them to do their job.
-    """).replace("\n", " ") + prompt.messages[0].prompt.template
-
-prompt = prompt.partial(
-    tools=render_text_description_and_args(tools),
-    tool_names=", ".join([t.name for t in tools]),
-)
-
-
-model = ChatOpenAI(temperature=0, streaming=True, max_retries=5,
-                   model=OPENAI_GPT_MODEL, api_key=OPENAI_API_KEY)
-agent = (
-    {
-        "input": lambda x: x["input"],
-        "agent_scratchpad": lambda x: format_log_to_str(x["intermediate_steps"]),
-    }
-    | prompt
-    | llm.bind(stop=["Observation"])
-    # | map_reduce_chain  # TODO: Add map-reduce after LLM
-    | JSONAgentOutputParser()
-)
 
 conversational_memory = ConversationBufferWindowMemory(
     memory_key='chat_history',
@@ -157,18 +77,6 @@ agent_kwargs = {
     "early_stopping_method": 'generate',
 }
 
-agent_executor = initialize_agent(
-    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-    tools=tools,
-    llm=llm,
-    verbose=True,
-    max_iterations=5,
-    # memory=conversational_memory,
-    # agent_kwargs=agent_kwargs,
-    handle_parsing_errors=True,
-    callback_manager=BaseCallbackManager(
-        handlers=[custom_callback_handler]),
-)
 
 app = FastAPI()
 
@@ -205,9 +113,80 @@ async def listen_to_pubsub(pubsub):
 
 
 async def agent_stream(input_data: str, chat_id: str):
+
+    custom_callback_handler = StreamingRedisCallbackHandler(
+        redis_host=REDIS_HOST, redis_port=REDIS_PORT, redis_channel=chat_id)
+
+    mp_llm = ChatOpenAI(
+        temperature=0,
+        model=OPENAI_GPT_MODEL,
+        openai_api_key=OPENAI_API_KEY,
+        openai_organization=None,
+        max_retries=5,
+        streaming=True,
+        callbacks=[custom_callback_handler],
+    )
+
+    tools = [
+        MPThermoExpert(llm=mp_llm).as_tool(
+            agent_kwargs=dict(return_intermediate_steps=False)),
+        MPElasticityExpert(llm=mp_llm).as_tool(
+            agent_kwargs=dict(return_intermediate_steps=False)),
+        MPDielectricExpert(llm=mp_llm).as_tool(
+            agent_kwargs=dict(return_intermediate_steps=False)),
+        MPMagnetismExpert(llm=mp_llm).as_tool(
+            agent_kwargs=dict(return_intermediate_steps=False)),
+        MPElectronicExpert(llm=mp_llm).as_tool(
+            agent_kwargs=dict(return_intermediate_steps=False)),
+        MPPiezoelectricExpert(llm=mp_llm).as_tool(
+            agent_kwargs=dict(return_intermediate_steps=False)),
+        MPSummaryExpert(llm=mp_llm).as_tool(
+            agent_kwargs=dict(return_intermediate_steps=False)),
+        MPSynthesisExpert(llm=mp_llm).as_tool(
+            agent_kwargs=dict(return_intermediate_steps=False)),
+        MPStructureRetriever(llm=mp_llm).as_tool(
+            agent_kwargs=dict(return_intermediate_steps=True)),
+        arxiv,
+        wikipedia,
+    ]
+    prompt = hub.pull("hwchase17/react-multi-input-json")
+    prompt.messages[0].prompt.template = re.sub(
+        r"\s+", " ",
+        """You are a data-aware agent that can consult materials-related
+        data through Materials Project (MP) database, arXiv, and Wikipedia. Ask 
+        user to clarify their queries if needed. Please note that you don't have 
+        direct control over MP but through multiple assistant agents to help you. 
+        You need to provide complete context in the input for them to do their job.
+        """).replace("\n", " ") + prompt.messages[0].prompt.template
+
+    prompt = prompt.partial(
+        tools=render_text_description_and_args(tools),
+        tool_names=", ".join([t.name for t in tools]),
+    )
+
+    llm = ChatOpenAI(
+        temperature=0,
+        model=OPENAI_GPT_MODEL,
+        openai_api_key=OPENAI_API_KEY,
+        openai_organization=None,
+        streaming=True,
+        callbacks=[custom_callback_handler],
+    )
+
+    agent_executor = initialize_agent(
+        agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+        tools=tools,
+        llm=llm,
+        verbose=True,
+        max_iterations=5,
+        # memory=conversational_memory,
+        # agent_kwargs=agent_kwargs,
+        handle_parsing_errors=True,
+        callback_manager=BaseCallbackManager(
+            handlers=[custom_callback_handler]),
+    )
     pubsub = redis_client.pubsub()
-    # pubsub.subscribe(chat_id)
-    pubsub.subscribe('llm_stream')
+    pubsub.subscribe(chat_id)
     print("Subscribed to 'llm_stream' channel. Listening for new messages...")
 
     ainvoke_task = asyncio.create_task(
