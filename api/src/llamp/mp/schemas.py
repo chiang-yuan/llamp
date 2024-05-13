9cfb06367196ce92ba2af0282ad248a236e7a666
Yuan Chiang <cyrusyc@berkeley.edu>
2024-05-12 20:27:27 -0700

    improve elastic workflow

--- api/src/llamp/mp/schemas.py
+++ api/src/llamp/mp/schemas.py
@@ -22,8 +22,12 @@ class SummarySchema(BaseModel):
     )
     elements: str | None = Field(
         None,
-        description="Query by elements in the material composition as a comma-separated list",
+        description="Query structure including elements (NOT EXCLUSIVE) in composition. Use a comma-separated list",
     )
+    # elements: str | None = Field(
+    #     None,
+    #     description="Query by elements in the material composition as a comma-separated list",
+    # )
     exclude_elements: str | None = Field(
         None,
         description="Query by excluded elements in the material composition as a comma-separated list",
@@ -313,11 +317,15 @@ class SummarySchema(BaseModel):
 
 
 class StructureSchema(SummarySchema):
+    """Search schema for the search_materials_structure__get endpoint. 
+    If pure elemental structures are desired, use `formula` instead of `elements`.
+    """
+
     limit: int | None = Field(
         default=5, description="Maximum number of entries to return", requried=True
     )
     fields: str | None = Field(
-        default="material_id,structure,",
+        default="material_id,structure",
         description="Comma-delimited list of fields to return in results. Fields include: `builder_meta` `nsites` `elements` `nelements` `composition` `composition_reduced` `formula_pretty` `formula_anonymous` `chemsys` `volume` `density` `density_atomic` `symmetry` `property_name` `material_id` `deprecated` `deprecation_reasons` `last_updated` `origins` `warnings` `structure` `task_ids` `uncorrected_energy_per_atom` `energy_per_atom` `formation_energy_per_atom` `energy_above_hull` `is_stable` `equilibrium_reaction_energy_per_atom` `decomposes_to` `xas` `grain_boundaries` `band_gap` `cbm` `vbm` `efermi` `is_gap_direct` `is_metal` `es_source_calc_id` `bandstructure` `dos` `dos_energy_up` `dos_energy_down` `is_magnetic` `ordering` `total_magnetization` `total_magnetization_normalized_vol` `total_magnetization_normalized_formula_units` `num_magnetic_sites` `num_unique_magnetic_sites` `types_of_magnetic_species` `k_voigt` `k_reuss` `k_vrh` `g_voigt` `g_reuss` `g_vrh` `universal_anisotropy` `homogeneous_poisson` `e_total` `e_ionic` `e_electronic` `n` `e_ij_max` `weighted_surface_energy_EV_PER_ANG2` `weighted_surface_energy` `weighted_work_function` `surface_anisotropy` `shape_factor` `has_reconstructed` `possible_species` `has_props` `theoretical` `database_IDs`",
     )
     return_mode: str | None = Field(
@@ -410,7 +418,7 @@ class ElasticitySchema(BaseModel):
         description="Max number of entries to return in a single query. Limited to 1000.",
     )
     fields: str | None = Field(
-        default="material_id,formula_pretty,elastic_tensor",
+        default="material_id,formula_pretty,elastic_tensor,bulk_modulus,shear_modulus,sound_velocity,thermal_conductivity,young_modulus,universal_anisotropy,homogeneous_poisson,debye_temperature",
         description="Fields to project from ElasticityDoc as a list of comma separated strings. Fields include: 'material_id' 'formula_pretty' 'elastic_tensor' 'builder_meta', 'nsites', 'elements', 'nelements', 'composition', 'composition_reduced', 'formula_anonymous', 'chemsys', 'volume', 'density', 'density_atomic', 'symmetry', 'property_name', 'deprecated', 'deprecation_reasons', 'last_updated', 'origins', 'warnings', 'structure', 'order', 'compliance_tensor', 'bulk_modulus', 'shear_modulus', 'sound_velocity', 'thermal_conductivity', 'young_modulus', 'universal_anisotropy', 'homogeneous_poisson', 'debye_temperature', 'fitting_data', 'fitting_method', 'state'",
     )
     all_fields: bool | None = Field(
